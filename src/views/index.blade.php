24ed0fca7990e2c5449db203afde2b540825dd79
Prasun Das <prasun.das@learningmate.com>
2023-06-13 22:23:02 +0530

    Design improvement and error handling

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -11,24 +11,41 @@
 
     <body>
         <div class="container">
-            <div class="card">
+            <div class="card m-5">
                 <div class="card-header text-center font-weight-bold">
                     <h1>Get Weather Info</h1>
                 </div>
-                
+
                 <div class="card-body">
-                    @if(isset($temp))
-                    <h3 class="text-center">Temperature: {{ $temp }}&deg;C</h3>
-                    @endif
-                    
-                    <form name="weather-info" id="weather-info" method="post" action="{{url('weather-info')}}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">City</label>
-                            <input type="text" id="city" name="city" class="form-control" placeholder="Enter city" required="">
+                    @if(isset($error))
+                    <div class="row p-3">
+                        <div class="col">
+                            <h3 class="text-center text-danger">{{ $error }}</h3>
                         </div>
-                        <button type="submit" class="btn btn-primary">Send Request</button>
-                    </form>
+                    </div>
+                    @endif
+                    @if(isset($temp))
+                    <div class="row p-3">
+                        <div class="col">
+                            <h3 class="text-center">Temperature: {{ $temp }}&deg;C</h3>
+                        </div>
+                    </div>
+                    @endif
+                    <div class="row">
+                        <div class="col">
+                            <form name="weather-info" id="weather-info" method="post" action="{{url('weather-info')}}">
+                                @csrf
+                                <div class="row form-group">
+                                    <div class="col">
+                                        <input type="text" id="city" name="city" class="form-control" placeholder="Enter City, State or Country" required="">
+                                    </div>
+                                    <div class="col">
+                                        <button type="submit" class="btn btn-primary">Send Request</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
